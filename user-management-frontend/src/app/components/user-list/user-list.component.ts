df8d9666cecf469fb6872ffbeb8b7f6d5ad6ffe9
root <root@localhost.localdomain>
2026-02-25 15:41:17 -0500

    feat: implement user list component

--- user-management-frontend/src/app/components/user-list/user-list.component.ts
+++ user-management-frontend/src/app/components/user-list/user-list.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  users: User[] = [];
+  loading = false;
+  error: string | null = null;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    this.loading = true;
+    this.error = null;
+    
+    this.userService.getUsers().subscribe({
+      next: (users) => {
+        this.users = users;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Error loading users';
+        this.loading = false;
+        console.error('Error loading users:', err);
+      }
+    });
+  }
+
+  deleteUser(id: string): void {
+    if (confirm('Are you sure you want to delete this user?')) {
+      this.userService.deleteUser(id).subscribe({
+        next: () => {
+          this.loadUsers();
+        },
+        error: (err) => {
+          this.error = 'Error deleting user';
+          console.error('Error deleting user:', err);
+        }
+      });
+    }
+  }
 }
